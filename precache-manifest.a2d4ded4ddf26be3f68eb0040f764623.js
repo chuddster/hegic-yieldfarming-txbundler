6cb012d459adebdc43c587579e09c94833d80f91
Jacob Chudnovsky <jchudnov@Jacobs-MacBook-Pro.local>
2021-02-02 16:55:07 -0800

    Updates

---
+++ precache-manifest.a2d4ded4ddf26be3f68eb0040f764623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49125ee9bf1ad005170e4fced828e1d",
+    "url": "/hegic-yieldfarming-txbundler/index.html"
+  },
+  {
+    "revision": "f66ac198db98a67d6945",
+    "url": "/hegic-yieldfarming-txbundler/static/css/main.6ed9ceba.chunk.css"
+  },
+  {
+    "revision": "3ef300e3fb40a1b0f4b0",
+    "url": "/hegic-yieldfarming-txbundler/static/js/2.eed4c374.chunk.js"
+  },
+  {
+    "revision": "119eda95dc740fedac00182dbb1baca0",
+    "url": "/hegic-yieldfarming-txbundler/static/js/2.eed4c374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66ac198db98a67d6945",
+    "url": "/hegic-yieldfarming-txbundler/static/js/main.4b3a7030.chunk.js"
+  },
+  {
+    "revision": "545f9a49f3346b72bf9f",
+    "url": "/hegic-yieldfarming-txbundler/static/js/runtime-main.f76c4e19.js"
+  },
+  {
+    "revision": "562753a3e9c2a0e00a8f56bdd1e4aa1f",
+    "url": "/hegic-yieldfarming-txbundler/static/media/medium-brands.562753a3.svg"
+  }
+]);
